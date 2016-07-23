8817fcb1a477b555b7e15e604a1ff499864d3e3d
Fatima Hammami <en.f.h@live.com>
2016-07-23 20:52:51 +0300

    n

--- client/app/profile/profile.js
+++
@@ -1,9 +0,0 @@
-angular.module('Memorize.profile',[])
-.controller('profileController', function( $scope, Profile ) {
-	$scope.profile={};
-
-	Profile.showProfile().then(function(data){
-		$scope.profile.data=data.uservalue;
-		console.log($scope.profile.data);
-	})
-});
